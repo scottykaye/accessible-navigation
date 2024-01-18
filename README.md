2c0bf962cb5342b2ada3adf9259a08f95df4717c
Scott Kaye <skaye@wayfair.com>
2024-01-18 12:21:15 -0500

    First pass of adding the package

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# aria-keyboard-a11y
-A pub sub observer that handles keyboard navigation for tabs, accordions and menus
+# Aria Keyboard A11y
+
+A pub/sub observer that handles keyboard navigation for tabs, accordions and
+menus.
+
+This solution bundles the appropriate arrow navigation keys. It relies on
+browser native support for keyboard Tab, Shift+Tab as well as Enter and Spacebar
+behavior from HTML `<button />` elements.
+
+### Goal
+
+The drive behind the concept was to make the browser work smarter not harder.
+Leveraging everything we get natively for free from the browser and then add
+only the support that is missing. The solution abstracts away the functionality
+from the component to simplify usage.
+
+## Requirements
+
+This solution currently recommends and supports native HTML button elements to
+get full access to the appropriate supported keyboard events.a
+
+TODO: Confirm <a/> tag support
+
+### KeyboardNav
+
+A pub/sub class observer that monitors the elements provided to handle the
+keyboard events.
+
+#### Methods
+
+- `subscribe` - A method that takes a unique `label` and `element` arguments to
+  construct elements that are subscribed to the navigation events.
+- `unsubscribe` - A method that takes the unique `label` as an argument and
+  unsubscribes from the item being observed.
+- `update` - A method that takes two arguments the `event` and `label`
+  arguments. These trigger the navigation event. Recommended to be used with
+  `onKeyDown`.
+- `view` - A method used for console logging the elements being observed.
+
+## Use with React
+
+```jsx
+import { createKeyboardNav, KeyboardNav } from 'aria-keyboard-a11y'
+const tabs = KeyboardNav('horizontal')
+const useKeyboardNav = createKeyboardNav(tabs)
+
+function Tab({ label, title, setActiveTabs, isSelected = false, index = 0 }) {
+  function handleKeyDown(event) {
+    tabs.update(event, label)
+  }
+  const refs = useKeyboardNav(label)
+
+  return (
+    <>
+      <button
+        ref={refs}
+        className={`tab ${isSelected ? 'isSelected' : ''}`}
+        type="button"
+        onClick={handleClick}
+        role="tab"
+        aria-selected={isSelected}
+        aria-controls={`tabpanel-${index}`}
+        id={`tab-${index}`}
+        onKeyDown={handleKeyDown}
+      >
+        {title}
+      </button>
+    </>
+  )
+}
+```
+
+## Usage with native html
---
+++ index.ts
@@ -0,0 +1,114 @@
+import { useCallback } from 'react'
+
+const SUPPORTED_KEYS = {
+  ARROW_UP: 'ArrowUp',
+  ARROW_DOWN: 'ArrowDown',
+  ARROW_LEFT: 'ArrowLeft',
+  ARROW_RIGHT: 'ArrowRight',
+  HOME: 'Home',
+  END: 'End',
+}
+
+// type SupportedKeys = typeof SUPPORTED_KEYS[keyof typeof SUPPORTED_KEYS]
+
+export class KeyboardNav {
+  readonly orientation?: 'vertical' | 'horizontal'
+  observers: Record<string, HTMLElement>
+
+  constructor(orientation: 'vertical' | 'horizontal' = 'vertical') {
+    this.orientation = orientation
+    this.observers = {}
+  }
+
+  view() {
+    console.log(this.observers)
+  }
+
+  subscribe(label: string, element: HTMLElement): void {
+    this.observers = { ...this.observers, ...{ [label]: element } } as Record<
+      string,
+      HTMLElement
+    >
+  }
+
+  unsubscribe(label: string): void {
+    delete this.observers[label]
+  }
+
+  update(event: KeyboardEvent, current: string): void {
+    const labelList = Object.keys(this.observers)
+    const currentNumber = labelList.findIndex((item) => item === current)
+    const firstItem = 0
+    const lastItem = labelList.length - 1
+    const moveUp = currentNumber > firstItem ? currentNumber - 1 : lastItem
+    const moveDown = currentNumber < lastItem ? currentNumber + 1 : firstItem
+
+    // Expect event.key to be only one of what we support in the list above
+    if (Object.values(SUPPORTED_KEYS).includes(event.key)) {
+      event.preventDefault()
+      switch (event.key) {
+        case SUPPORTED_KEYS.HOME:
+          this.observers[labelList[firstItem]].focus()
+          break
+        case SUPPORTED_KEYS.END:
+          this.observers[labelList[lastItem]].focus()
+          break
+      }
+      if (this.orientation === 'vertical') {
+        switch (event.key) {
+          case SUPPORTED_KEYS.ARROW_UP:
+            this.observers[labelList[moveUp]].focus()
+            break
+          case SUPPORTED_KEYS.ARROW_DOWN:
+            this.observers[labelList[moveDown]].focus()
+            break
+        }
+      }
+      if (this.orientation === 'horizontal') {
+        switch (event.key) {
+          case SUPPORTED_KEYS.ARROW_LEFT:
+            this.observers[labelList[moveUp]].focus()
+            break
+          case SUPPORTED_KEYS.ARROW_RIGHT:
+            this.observers[labelList[moveDown]].focus()
+            break
+        }
+      }
+    }
+  }
+}
+
+interface RefObject<T> {
+  // Not readonly because we manipulate it with mergeRefs
+  current: T | null
+}
+
+const mergeRefs = (...refs: Array<RefObject<HTMLElement>>) => {
+  return (node: HTMLElement) => {
+    for (const ref of refs) {
+      ref.current = node
+    }
+  }
+}
+
+export function createKeyboardNavHook(instance: KeyboardNav) {
+  return function useKeyboardNav(
+    label: string,
+    parentRef: RefObject<HTMLElement>,
+  ) {
+    const refs = useCallback(
+      (node: HTMLElement) => {
+        if (parentRef) {
+          mergeRefs(parentRef)(node)
+        }
+        if (node === null) {
+          instance.unsubscribe(label)
+        } else {
+          instance.subscribe(label, node)
+        }
+      },
+      [instance.subscribe, instance.unsubscribe, parentRef, label],
+    )
+    return refs
+  }
+}
