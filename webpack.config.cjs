a4fa58fd6cdde7bbbd1ab3d8891110cadce868d3
Scott Kaye <skaye@wayfair.com>
2024-01-31 12:14:41 -0500

    Added a Demo space to next add navigation

---
+++ webpack.config.cjs
@@ -0,0 +1,24 @@
+// webpack.config.js
+const path = require('path')
+const htmlwebpackplugin = require('html-webpack-plugin')
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  plugins: [new htmlwebpackplugin({ template: './index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+}
